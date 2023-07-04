86ccb066c75f4940f9a976c48afcc24ed57704f2
https://github.com/OlimovBek <behzodbekolimov@gmail.com>
2023-07-04 15:08:09 +0500

    init

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from "../Button/Button";
+import './ProductItem.css'
+
+const ProductItem = (product, className, onAdd) => {
+    const onAddHandler = () => {
+        onAdd(product)
+    }
+    return (
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Narxi: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Savatchaga qo'shish
+            </Button>
+
+        </div>
+    );
+};
+
+export default ProductItem;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products = [
+    {id: '1', title: 'Jinsi', price: 120000, description: "Ko'k rangli tekis"},
+    {id: '2', title: 'Kurtka', price: 300000, description: "Ko'k rangli issiq"},
+    {id: '3', title: 'Jinsi 2', price: 140000, description: "Yashil rangli tekis"},
+    {id: '4', title: 'Kurtka 2', price: 350000, description: "Sariq rangli issiq"},
+    {id: '5', title: 'Jinsi 3', price: 125000, description: "Ko'k rangli tekis"},
+]
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if (alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+        setAddedItems(newItems);
+
+        if (newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Sotib olish ${getTotalPrice(newItems)}`
+            })
+        }
+    }
     return (
-        <div>
-            Product list
+        <div className={'list'}>
+            {products.map(item => (
+
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    className={'item'}
+                />
+            ))}
         </div>
     );
 };
